f3b858e15205348e708ededa7609719a1be73b20
charlescrabtree <charles.crabtree.horn@gmail.com>
2023-01-24 11:10:17 -0800

    finished setup, puzzling what needs to be done in comments

---
+++ index.js
@@ -0,0 +1,42 @@
+const process = require('process');
+
+let itemCostInput = null;
+let paymentInput = null;
+
+for(let i = 0; i < process.argv.length; ++1) {
+  const arg = process.argv[i];
+  if(arg === '--item-cost') {
+    itemCostInput = process.argv[i+1];
+    ++i;
+  } else if(arg === '--payment') {
+    paymentInput = process.argv[i+1];
+    ++i;
+  }
+}
+
+if(itemCostInput == null) {
+  console.error('--item-cost must be provided');
+  process.exit(1);
+}
+
+const itemCost = Number(itemCostInput) * 100;
+if(isNaN(itemCost)) {
+  console.log('--item-cost must be a number');
+  process.exit(1);
+}
+
+if(paymentInput == null) {
+  console.error('--payment must be provided');
+  process.exit(1);
+}
+
+const payment = Number(paymentInput) * 100;
+if(isNaN(payment)) {
+  console.log('--payment must be a number');
+  process.exit(1);
+}
+
+// Need to add quarters === 25
+// Dimes === 10
+// Nickels === 5
+// Dollars === 100
